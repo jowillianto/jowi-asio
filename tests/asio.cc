d0bc1fa78b854b3d0dd61ef62e303b46eda4b790
Jonathan Willianto <jowillianto@gmail.com>
2025-09-23 09:58:07 +0900

    [#main] add task awaitable

--- tests/asio.cc
+++ tests/asio.cc
@@ -1,5 +1,6 @@
 #include <jowi/test_lib.hpp>
 #include <chrono>
+#include <coroutine>
 #include <functional>
 import jowi.test_lib;
 import jowi.asio;
@@ -11,9 +12,8 @@ JOWI_SETUP(argc, argv) {
   test_lib::get_test_context().set_time_unit(test_lib::test_time_unit::MILLI_SECONDS);
 }
 
-asio::basic_task<int> random_sleep(uint32_t delay) {
+asio::basic_task<void> random_sleep(uint32_t delay) {
   co_await asio::sleep_for(delay);
-  co_return 0;
 }
 
 template <class F, class... Args>
@@ -27,11 +27,22 @@ std::pair<std::invoke_result_t<F, Args...>, std::chrono::system_clock::duration>
   return std::pair{std::move(res), end - beg};
 }
 
-JOWI_ADD_TEST(test_async_fibonacci) {
+JOWI_ADD_TEST(test_async_sleep) {
   auto beg = std::chrono::system_clock::now();
   asio::gather(random_sleep(500), random_sleep(500));
   auto end = std::chrono::system_clock::now();
   test_lib::assert_true(
-    500 <= std::chrono::duration_cast<std::chrono::milliseconds>(end - beg).count()
+    std::chrono::duration_cast<std::chrono::milliseconds>(end - beg).count() <= 510
+  );
+}
+
+JOWI_ADD_TEST(test_async_task_await) {
+  /* Nested Random Sleep. This should run in parallel with random_sleep */
+  auto nested_f = [](uint32_t delay) -> asio::basic_task<void> { co_await random_sleep(delay); };
+  auto beg = std::chrono::system_clock::now();
+  asio::gather(nested_f(500), random_sleep(500));
+  auto end = std::chrono::system_clock::now();
+  test_lib::assert_true(
+    std::chrono::duration_cast<std::chrono::milliseconds>(end - beg).count() <= 510
   );
 }
